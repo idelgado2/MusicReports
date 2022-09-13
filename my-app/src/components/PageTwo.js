1b119bf301e7571a94139902ee49fa19d224ea85
Isaac Delgado <isaacdelgado1994@gmail.com>
2022-09-13 04:27:13 -0700

    adding table to page two

--- my-app/src/components/PageTwo.js
+++ my-app/src/components/PageTwo.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom"
+import { MyTable } from './MyTable';
+
   
 const PageTwo = () => {
   const navigate = useNavigate();
   
   return (
   <>
-     <h1>PageTwo</h1>
+     <h1>Page Two</h1>
      <button onClick={()=>navigate(-1)}>Go To PageOne</button>
+     <MyTable/>
   </>
   )
 };
