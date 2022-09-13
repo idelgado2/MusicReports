537327b2f2c1008a5fa1cd9485d7875e3db794ba
Isaac Delgado <isaacdelgado1994@gmail.com>
2022-09-13 02:27:43 -0700

    adding page 1

---
+++ my-app/src/page1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+class Game extends React.Component {
+    render(){
+        return(
+            <h1>Hello!</h1>
+        );
+    }
+}
+
+export default Game;
